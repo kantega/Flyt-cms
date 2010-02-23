54568887d0f05bdd210aac39bdf0af4752a68ac2
Anders Skar <Anders.Skar@kantega.no>
2010-02-23 12:14:29 +0000

    Rating system
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@872 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/rating/src/java/no/kantega/publishing/rating/dao/JdbcRatingDao.java
+++ modules/rating/src/java/no/kantega/publishing/rating/dao/JdbcRatingDao.java
@@ -2,6 +2,7 @@ package no.kantega.publishing.rating.dao;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.PreparedStatementCreator;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import javax.sql.DataSource;
@@ -11,17 +12,18 @@ import no.kantega.publishing.api.rating.Rating;
 import java.util.List;
 import java.sql.SQLException;
 import java.sql.ResultSet;
+import java.sql.PreparedStatement;
+import java.sql.Connection;
 
 /**
  *
  */
 public class JdbcRatingDao extends JdbcDaoSupport implements RatingDao {
-    private DataSource dataSource;
     private RatingRowMapper ratingRowMapper = new RatingRowMapper();
 
+    @SuppressWarnings("unchecked")
     public List<Rating> getRatingsForObject(String objectId, String context) {
-
-        return null;
+        return getJdbcTemplate().query("select * from ratings where objectId = ? and context = ? order by ratingDate desc", new Object[] {objectId, context}, ratingRowMapper);
     }
 
 
@@ -30,13 +32,26 @@ public class JdbcRatingDao extends JdbcDaoSupport implements RatingDao {
     }
 
 
-    public void saveOrUpdateRating(Rating rating) {
+    public void saveOrUpdateRating(final Rating r) {
+        getJdbcTemplate().update("delete from ratings where objectId = ? and context = ? and userId = ?", new Object[] {r.getObjectId(), r.getContext(), r.getUserid()});
+
+        getJdbcTemplate().update(new PreparedStatementCreator() {
+            public PreparedStatement createPreparedStatement(Connection c) throws SQLException {
+                PreparedStatement st = c.prepareStatement("insert into ratings values(?,?,?,?,?)");
+                st.setString(1, r.getUserid());
+                st.setString(2, r.getObjectId());
+                st.setString(3, r.getContext());
+                st.setInt(4, r.getRating());
+                st.setTimestamp(5, new java.sql.Timestamp(r.getDate().getTime()));
+                return st;
+            }
+        });
 
     }
 
-
+    @SuppressWarnings("unchecked")
     public List<Rating> getRatingsForUser(String userId) {        
-        return null;
+        return getJdbcTemplate().query("select * from ratings where userId = ? order by ratingDate desc", new Object[] {userId}, ratingRowMapper);
     }
 
     private class RatingRowMapper implements RowMapper {
