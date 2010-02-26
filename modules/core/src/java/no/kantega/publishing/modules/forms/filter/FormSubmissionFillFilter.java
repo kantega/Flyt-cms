dff640d83744b96b64da5fac71c46fe41bc95e8f
tarkil <tarkil@kantega.no>
2010-02-26 11:31:45 +0000

    Fixed bug in regular expression-validator
    git-svn-id: https://opensource.kantega.no/svn/openaksess/branches/openaksess-6.1.x@893 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/modules/forms/filter/FormSubmissionFillFilter.java
+++ modules/core/src/java/no/kantega/publishing/modules/forms/filter/FormSubmissionFillFilter.java
@@ -25,7 +25,7 @@ public class FormSubmissionFillFilter extends XMLFilterImpl {
     private int currentFieldIndex;
     private FormElementValidatorFactory formElementValidatorFactory;
     private boolean capture = false;
-    private StringBuilder validatorArg = new StringBuilder();
+    private StringBuilder validatorArg;
 
     public FormSubmissionFillFilter(Map<String, String[]> params, Form form) {
         this.params = params;
@@ -86,9 +86,12 @@ public class FormSubmissionFillFilter extends XMLFilterImpl {
     }
 
     private void processSpan(String name, Attributes attributes) {
-        capture = attributes != null
+        if (attributes != null
                 && attributes.getValue("class") != null
-                && attributes.getValue("class").contains("regex");
+                && attributes.getValue("class").contains("regex")) {
+            capture = true;
+            validatorArg = new StringBuilder();
+        }
     }
 
     private void processInput(String name, Attributes attributes) {
@@ -136,7 +139,6 @@ public class FormSubmissionFillFilter extends XMLFilterImpl {
             formSubmission.getErrors().add(new FormError(inputName, currentFieldIndex, "aksess.formerror.mandatory"));
         }
 
-        validatorArg = new StringBuilder();
         mandatory = false;
     }
 
