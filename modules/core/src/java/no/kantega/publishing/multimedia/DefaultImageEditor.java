be4d1078ebee0acc77a7ad3b6c4d5d43f370442a
Anders Skar <Anders.Skar@kantega.no>
2010-04-19 13:16:08 +0000

    AP-806
    git-svn-id: https://opensource.kantega.no/svn/openaksess/branches/openaksess-6.2.x@1260 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/multimedia/DefaultImageEditor.java
+++ modules/core/src/java/no/kantega/publishing/multimedia/DefaultImageEditor.java
@@ -80,6 +80,7 @@ public class DefaultImageEditor implements ImageEditor {
                     Graphics2D graphics2D = cropImage.createGraphics();
                     graphics2D.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
                     graphics2D.drawImage(resizedImage, -cropx, -cropy, resizedImage.getWidth(), resizedImage.getHeight(), null);
+                    resizedImage = cropImage;
                 }
 
                 // Write image
