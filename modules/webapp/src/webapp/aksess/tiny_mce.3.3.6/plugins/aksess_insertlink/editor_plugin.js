8ee37bd9d7edfff13d9853bbba31380dcbeefe0b
Anders Skar <Anders.Skar@kantega.no>
2010-05-21 11:59:03 +0000

    Added Tinymce 3.3.6
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@1419 fd808399-8219-4f14-9d4c-37719d9ec93d

---
+++ modules/webapp/src/webapp/aksess/tiny_mce.3.3.6/plugins/aksess_insertlink/editor_plugin.js
@@ -0,0 +1,69 @@
+(function() {
+    // Load plugin specific language pack - not needed
+//    tinymce.PluginManager.requireLangPack('aksess_insertlink');
+
+	tinymce.create('tinymce.plugins.InsertLink', {
+		init : function(ed, url) {
+            this.editor = ed;
+            this.url = url;
+
+			// Register command
+            ed.addCommand('insertLinkCmd', this._openPopup, this);
+
+			// Register button
+			ed.addButton('link', {
+                title : 'advlink.link_desc',
+                cmd : 'insertLinkCmd'
+            });
+
+            // Register shortcut
+            ed.addShortcut('ctrl+k', 'advlink.advlink_desc', 'insertLinkCmd');
+		},
+
+		getInfo : function() {
+			return {
+				longname : 'Insert Link',
+				author : 'Kantega AS',
+				authorurl : 'http://www.kantega.no',
+				version : tinymce.majorVersion + "." + tinymce.minorVersion
+			};
+		},
+
+        _openPopup : function() {
+            var se = this.editor.selection;
+            // No selection and not in link
+            if (se.isCollapsed() && !this.editor.dom.getParent(se.getNode(), 'A')) {
+                return;
+            }
+
+            var href = '';
+            var anchor = '';
+            var newWindow = false;
+            var elm = this.editor.selection.getNode();
+            elm = this.editor.dom.getParent(elm, "A");
+            if (elm != null && elm.nodeName == "A") {
+                href = this.editor.dom.getAttrib(elm, 'href');
+                if (href.indexOf("#") != -1) {
+                    var hrefSplit = href.split("#");
+                    href = hrefSplit[0];
+                    anchor = hrefSplit[1];
+                }
+                var onclick = this.editor.dom.getAttrib(elm, 'onclick');
+                if (onclick.indexOf('window.open') != -1) {
+                    newWindow = true;
+                }
+            }
+
+            openaksess.common.modalWindow.open({
+                title:"Sett inn lenke",
+                iframe:true,
+                href: "popups/InsertLink.action?url=" + encodeURI(href) + "&anchor=" + encodeURI(anchor) + "&isOpenInNewWindow=" + encodeURI(newWindow),
+                width: 600,
+                height:300});
+        }
+
+	});
+
+	// Register plugin
+	tinymce.PluginManager.add('aksess_insertlink', tinymce.plugins.InsertLink);
+})();
