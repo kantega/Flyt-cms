495fb531534a75a95deee01eedd7e9689e3501f1
Anders Skar <Anders.Skar@kantega.no>
2010-05-28 07:50:05 +0000

    Webdav support
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@1446 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/webdav/src/java/no/kantega/publishing/webdav/AksessResourceFactory.java
+++ modules/webdav/src/java/no/kantega/publishing/webdav/AksessResourceFactory.java
@@ -2,7 +2,10 @@ package no.kantega.publishing.webdav;
 
 import com.bradmcevoy.http.ResourceFactory;
 import com.bradmcevoy.http.Resource;
+import no.kantega.commons.configuration.Configuration;
+import no.kantega.commons.exception.ConfigurationException;
 import no.kantega.commons.log.Log;
+import no.kantega.publishing.common.Aksess;
 import no.kantega.publishing.spring.RootContext;
 import no.kantega.publishing.webdav.resourcehandlers.AksessWebDavResourceHandler;
 
@@ -16,16 +19,29 @@ public class AksessResourceFactory implements ResourceFactory {
     private final static String CONTENT = "/content";
     private final static String MULTIMEDIA = "/multimedia";
 
+    private Collection<AksessWebDavResourceHandler> resourceHandlers = null;
+
     public Resource getResource(String host, String path) {
-        Log.debug(this.getClass().getName(), "Get resource: " + path);
+        try {
+            Configuration config = Aksess.getConfiguration();
+            if (config.getBoolean("webdav.enabled", false)) {
+                Log.debug(this.getClass().getName(), "Get resource: " + path);
 
-        path = path.substring(path.indexOf(ROOT) +  + ROOT.length(), path.length());
+                path = path.substring(path.indexOf(ROOT) +  + ROOT.length(), path.length());
 
-        Collection<AksessWebDavResourceHandler> resourceHandlers = RootContext.getInstance().getBeansOfType(AksessWebDavResourceHandler.class).values();
-        for (AksessWebDavResourceHandler resourceHandler : resourceHandlers) {
-            if (resourceHandler.canHandlePath(path)) {
-                return resourceHandler.getResourceFromPath(path);
+                if (resourceHandlers == null) {
+                    resourceHandlers = RootContext.getInstance().getBeansOfType(AksessWebDavResourceHandler.class).values();
+                }
+
+                for (AksessWebDavResourceHandler resourceHandler : resourceHandlers) {
+                    if (resourceHandler.canHandlePath(path)) {
+                        return resourceHandler.getResourceFromPath(path);
+                    }
+                }
             }
+
+        } catch (ConfigurationException e) {
+            Log.error(this.getClass().getName(), e, null, null);
         }
 
         return null;
