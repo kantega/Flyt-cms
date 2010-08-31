bbbbe417c4b7a7a315f008878d892ede5f20210b
Eirik Bjørsnøs <Eirik.Bjorsnos@kantega.no>
2010-08-31 12:47:25 +0000

    AP-911: Don't use 1.6 style @Override on methods implemented directly from interfaces
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@1729 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/admin/taglib/expires/DigestResourceKeyProvider.java
+++ modules/core/src/java/no/kantega/publishing/admin/taglib/expires/DigestResourceKeyProvider.java
@@ -136,7 +136,6 @@ public class DigestResourceKeyProvider implements ResourceKeyProvider, ServletCo
         }
 
 
-        @Override
         public ServletOutputStream getOutputStream() throws IOException {
             return new ServletOutputStream() {
                 @Override
@@ -156,90 +155,69 @@ public class DigestResourceKeyProvider implements ResourceKeyProvider, ServletCo
             };
         }
 
-        @Override
         public PrintWriter getWriter() throws IOException {
             return new PrintWriter(getOutputStream());
         }
 
-        @Override
         public void setCharacterEncoding(String charset) {
 
         }
 
-        @Override
         public void setContentLength(int len) {
         }
 
-        @Override
         public void setContentType(String type) {
         }
 
-        @Override
         public void setBufferSize(int size) {
         }
 
-        @Override
         public void flushBuffer() throws IOException {
         }
 
-        @Override
         public void reset() {
         }
 
-        @Override
         public void resetBuffer() {
         }
 
-        @Override
         public void setLocale(Locale loc) {
         }
 
-        @Override
         public void addCookie(Cookie cookie) {
 
         }
 
-        @Override
         public void sendError(int sc, String msg) throws IOException {
         }
 
-        @Override
         public void sendError(int sc) throws IOException {
         }
 
-        @Override
         public void sendRedirect(String location) throws IOException {
         }
 
-        @Override
         public void setDateHeader(String name, long date) {
         }
 
-        @Override
         public void addDateHeader(String name, long date) {
         }
 
-        @Override
         public void setHeader(String name, String value) {
         }
 
-        @Override
         public void addHeader(String name, String value) {
         }
 
-        @Override
         public void setIntHeader(String name, int value) {
         }
 
-        @Override
         public void addIntHeader(String name, int value) {
         }
 
-        @Override
         public void setStatus(int sc) {
         }
 
-        @Override
         public void setStatus(int sc, String sm) {
         }
 
