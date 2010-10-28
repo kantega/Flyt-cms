0d63c16d0c519c41cd98f8a06c344481a647e70a
Eirik Bjørsnøs <Eirik.Bjorsnos@kantega.no>
2010-10-28 09:14:27 +0000

    Use jDOM instead of JSP to produce XML for smoke tests
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@1997 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/controls/smoketest/SmokeTestPagesController.java
+++ modules/core/src/java/no/kantega/publishing/controls/smoketest/SmokeTestPagesController.java
@@ -1,17 +1,23 @@
 package no.kantega.publishing.controls.smoketest;
 
+import no.kantega.publishing.api.taglibs.content.GetAttributeCommand;
+import no.kantega.publishing.api.taglibs.content.util.AttributeTagHelper;
 import no.kantega.publishing.common.cache.TemplateConfigurationCache;
 import no.kantega.publishing.common.data.Content;
 import no.kantega.publishing.common.data.ContentQuery;
 import no.kantega.publishing.common.data.DisplayTemplate;
 import no.kantega.publishing.common.data.SortOrder;
+import no.kantega.publishing.common.data.enums.AttributeDataType;
 import no.kantega.publishing.common.data.enums.ContentProperty;
 import no.kantega.publishing.common.service.ContentManagementService;
+import org.jdom.Element;
+import org.jdom.output.XMLOutputter;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,12 +28,10 @@ public class SmokeTestPagesController extends AbstractController {
 
     private TemplateConfigurationCache templateConfigurationCache;
 
-    private String view;
 
     @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        ModelAndView model = new ModelAndView(view);
 
         List<Content> contents = new ArrayList<Content>();
 
@@ -41,19 +45,43 @@ public class SmokeTestPagesController extends AbstractController {
 
                 contents.addAll(cms.getContentList(query, 1, new SortOrder(ContentProperty.PUBLISH_DATE)));
             }
-            model.addObject("pages", contents);
+            Element pages = new Element("pages");
+
+            for(Content content : contents) {
+                Element page = new Element("page");
+                final GetAttributeCommand cmd = new GetAttributeCommand();
+                cmd.setAttributeType(AttributeDataType.CONTENT_DATA);
+
+                cmd.setName("url");
+                page.setAttribute("url", AttributeTagHelper.getAttribute(content, cmd).substring(request.getContextPath().length()));
+
+                cmd.setName("displaytemplate");
+                page.setAttribute("category", AttributeTagHelper.getAttribute(content, cmd));
+
+                page.setAttribute("title", content.getTitle());
+
+                page.setAttribute("id", "contentId-" + content.getId());
+
+                pages.addContent(page);
+            }
+
+            response.setContentType("text/xml");
+            XMLOutputter outputter = new XMLOutputter();
+            try {
+                outputter.output(pages, response.getOutputStream());
+            }
+            catch (IOException e) {
+                System.err.println(e);
+            }
         } else {
             response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
         }
 
-        return model;
+        return null;
     }
 
     public void setTemplateConfigurationCache(TemplateConfigurationCache templateConfigurationCache) {
         this.templateConfigurationCache = templateConfigurationCache;
     }
 
-    public void setView(String view) {
-        this.view = view;
-    }
 }
