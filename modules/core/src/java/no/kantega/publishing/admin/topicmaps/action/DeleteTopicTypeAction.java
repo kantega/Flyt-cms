70c9d94989bdeee140d2f2693d8608a0bde19537
Harald  Evensen <Harald.Evensen@kantega.no>
2011-02-03 09:44:02 +0000

    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@2422 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/admin/topicmaps/action/DeleteTopicTypeAction.java
+++ modules/core/src/java/no/kantega/publishing/admin/topicmaps/action/DeleteTopicTypeAction.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2009 Kantega AS
+ * Copyright 2011 Kantega AS
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
