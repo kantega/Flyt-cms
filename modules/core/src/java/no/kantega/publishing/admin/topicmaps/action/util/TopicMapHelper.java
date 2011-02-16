77e2e7ab8a70df93826c791241865c897c3a0db9
Anders Skar <Anders.Skar@kantega.no>
2011-02-16 07:39:39 +0000

    AP-1131
    git-svn-id: https://opensource.kantega.no/svn/openaksess/branches/openaksess-7.1.x@2518 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/admin/topicmaps/action/util/TopicMapHelper.java
+++ modules/core/src/java/no/kantega/publishing/admin/topicmaps/action/util/TopicMapHelper.java
@@ -16,23 +16,12 @@
 
 package no.kantega.publishing.admin.topicmaps.action.util;
 
-import no.kantega.commons.exception.RegExpSyntaxException;
-import no.kantega.commons.log.Log;
-import no.kantega.commons.util.RegExp;
+import no.kantega.publishing.common.util.PrettyURLEncoder;
 
 public class TopicMapHelper {
-    private static String SOURCE = "TopicMapHelper";
-
     public static String createTopicIdFromName(String name) {
         String id = name.toLowerCase();
-        id = id.replace('æ', 'e');
-        id = id.replace('ø', 'o');
-        id = id.replace('å', 'a');
-        try {
-            id = RegExp.replace("[^a-z0-9]", id, "");
-        } catch (RegExpSyntaxException e) {
-            Log.error(SOURCE, "createTopicIdFromName(): " + name);
-        }
-        return id;
+
+        return PrettyURLEncoder.encode(id);
     }
 }
