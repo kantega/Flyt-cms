3ab41f939f7a53299452d804f5adfa271d660a10
Jørund Leknes <Jorund.Leknes@kantega.no>
2011-03-08 13:24:17 +0000

    Merge from trunk - AP-1152
    git-svn-id: https://opensource.kantega.no/svn/openaksess/branches/openaksess-7.1.x@2706 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/test/java/no/kantega/publishing/multimedia/metadata/JpegImageMetadataExtractorTest.java
+++ modules/core/src/test/java/no/kantega/publishing/multimedia/metadata/JpegImageMetadataExtractorTest.java
@@ -84,15 +84,8 @@ public class JpegImageMetadataExtractorTest {
     }
 
     @Test
-    public void shouldGetAllExifData() {
-        Multimedia multimediaWithMetadata = metadataExtractor.extractMetadata(multimediaWithoutMetadata2);
-        assertEquals(66, multimediaWithMetadata.getExifMetadata().size());
-    }
-
-    @Test
     public void shouldExtractIptcCopyright() {
         Multimedia multimediaWithMetadata = metadataExtractor.extractMetadata(multimediaWithoutMetadata2);
         assertEquals("OpenAksessTest", multimediaWithMetadata.getAuthor());
     }
-
 }
