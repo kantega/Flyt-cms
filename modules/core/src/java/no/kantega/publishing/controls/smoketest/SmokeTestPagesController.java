674f378201075810acad53655a7feb6f4621de76
Kristian Lier Selnæs <kristian.selnaes@kantega.no>
2011-03-31 10:05:18 +0000

    Rolled back ignore template strategy
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@2779 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/controls/smoketest/SmokeTestPagesController.java
+++ modules/core/src/java/no/kantega/publishing/controls/smoketest/SmokeTestPagesController.java
@@ -1,7 +1,5 @@
 package no.kantega.publishing.controls.smoketest;
 
-import no.kantega.commons.client.util.RequestParameters;
-import no.kantega.commons.log.Log;
 import no.kantega.publishing.api.taglibs.content.GetAttributeCommand;
 import no.kantega.publishing.api.taglibs.content.util.AttributeTagHelper;
 import no.kantega.publishing.common.cache.TemplateConfigurationCache;
@@ -11,7 +9,6 @@ import no.kantega.publishing.common.data.DisplayTemplate;
 import no.kantega.publishing.common.data.SortOrder;
 import no.kantega.publishing.common.data.enums.AttributeDataType;
 import no.kantega.publishing.common.data.enums.ContentProperty;
-import no.kantega.publishing.common.data.enums.ContentType;
 import no.kantega.publishing.common.service.ContentManagementService;
 import org.jdom.Element;
 import org.jdom.output.XMLOutputter;
@@ -21,7 +18,8 @@ import org.springframework.web.servlet.mvc.AbstractController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -34,19 +32,6 @@ public class SmokeTestPagesController extends AbstractController {
     @Override
     protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
-        List<Integer> excludedTemplates = Collections.emptyList();
-        String excludedTemplatesParam = request.getParameter("excludedTemplates");
-        if (excludedTemplatesParam != null && excludedTemplatesParam.trim().length() > 0) {
-            String[] excludedIds = excludedTemplatesParam.split(",");
-            excludedTemplates = new ArrayList<Integer>();
-            for (String excludedId : excludedIds) {
-                try {
-                    excludedTemplates.add(Integer.parseInt(excludedId));
-                } catch (NumberFormatException e) {
-                    Log.info(this.getClass().getName(), "Could not exclude template with id: " + excludedId);
-                }
-            }
-        }
 
         List<Content> contents = new ArrayList<Content>();
 
@@ -61,27 +46,23 @@ public class SmokeTestPagesController extends AbstractController {
                 contents.addAll(cms.getContentList(query, 1, new SortOrder(ContentProperty.PUBLISH_DATE)));
             }
             Element pages = new Element("pages");
-            Element includes = new Element("includes");
-            pages.addContent(includes);
 
             for(Content content : contents) {
-                if (content.getType() == ContentType.PAGE && (excludedTemplates == null || !excludedTemplates.contains(content.getDisplayTemplateId()))) {
-                    Element page = new Element("page");
+                Element page = new Element("page");
+                final GetAttributeCommand cmd = new GetAttributeCommand();
+                cmd.setAttributeType(AttributeDataType.CONTENT_DATA);
 
-                    final GetAttributeCommand cmd = new GetAttributeCommand();
-                    cmd.setAttributeType(AttributeDataType.CONTENT_DATA);
-                    cmd.setName("url");
-                    page.setAttribute("url", AttributeTagHelper.getAttribute(content, cmd).substring(request.getContextPath().length()));
+                cmd.setName("url");
+                page.setAttribute("url", AttributeTagHelper.getAttribute(content, cmd).substring(request.getContextPath().length()));
 
-                    cmd.setName("displaytemplate");
-                    page.setAttribute("category", AttributeTagHelper.getAttribute(content, cmd));
+                cmd.setName("displaytemplate");
+                page.setAttribute("category", AttributeTagHelper.getAttribute(content, cmd));
 
-                    page.setAttribute("title", content.getTitle());
+                page.setAttribute("title", content.getTitle());
 
-                    page.setAttribute("id", "contentId-" + content.getId());
+                page.setAttribute("id", "contentId-" + content.getId());
 
-                    includes.addContent(page);
-                }
+                pages.addContent(page);
             }
 
             response.setContentType("text/xml");
@@ -90,7 +71,6 @@ public class SmokeTestPagesController extends AbstractController {
                 outputter.output(pages, response.getOutputStream());
             }
             catch (IOException e) {
-                Log.error(this.getClass().getName(), "An error occured when attempting to write smoke test config.", null, null);
                 System.err.println(e);
             }
         } else {
