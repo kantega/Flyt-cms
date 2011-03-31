adc31745449b09921a0dc8eff8d0c742a6271188
Kristian Lier Selnæs <kristian.selnaes@kantega.no>
2011-03-31 11:35:39 +0000

    Renamed the SmokeTestPagesController to the more generic TestPagesController as this controller is used by more than the smoketest
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@2781 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/controls/smoketest/TestPagesController.java
+++ modules/core/src/java/no/kantega/publishing/controls/smoketest/TestPagesController.java
@@ -10,6 +10,7 @@ import no.kantega.publishing.common.data.SortOrder;
 import no.kantega.publishing.common.data.enums.AttributeDataType;
 import no.kantega.publishing.common.data.enums.ContentProperty;
 import no.kantega.publishing.common.service.ContentManagementService;
+import org.apache.commons.lang.StringUtils;
 import org.jdom.Element;
 import org.jdom.output.XMLOutputter;
 import org.springframework.web.servlet.ModelAndView;
@@ -18,15 +19,21 @@ import org.springframework.web.servlet.mvc.AbstractController;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /**
- *
+ * Creates an xml containing a list of pages where each page is the published page for each display template.
+ * The xml produced:
+ * <pre>
+ * &lt;pages&gt;
+ *     &lt;page category="" title="" id="" url=""/&gt;
+ * &lt;/pages&gt;
+ * </pre>
  */
-public class SmokeTestPagesController extends AbstractController {
+public class TestPagesController extends AbstractController {
 
     private TemplateConfigurationCache templateConfigurationCache;
+    private static final String FILTER_SEPARATOR = "\\|";
 
 
     @Override
@@ -37,8 +44,11 @@ public class SmokeTestPagesController extends AbstractController {
 
         ContentManagementService cms = new ContentManagementService(request);
 
-        if("true".equals(getServletContext().getInitParameter("smokeTestEnabled"))) {
-            final List<DisplayTemplate> templates = templateConfigurationCache.getTemplateConfiguration().getDisplayTemplates();
+        if("true".equals(getServletContext().getInitParameter("testPagesEnabled"))) {
+            String excludeFilter = request.getParameter("excludeFilter");
+
+            List<DisplayTemplate> allTemplates = templateConfigurationCache.getTemplateConfiguration().getDisplayTemplates();
+            List<DisplayTemplate> templates = filterTemplates(allTemplates, excludeFilter);
             for (DisplayTemplate template : templates) {
                 ContentQuery query = new ContentQuery();
                 query.setDisplayTemplate(template.getId());
@@ -78,6 +88,48 @@ public class SmokeTestPagesController extends AbstractController {
         }
 
         return null;
+    }
+
+    private List<DisplayTemplate> filterTemplates(List<DisplayTemplate> allTemplates, String excludeFilter) {
+        Map<String, String> excludes = parseFilter(excludeFilter);
+
+        if (excludes.isEmpty()) {
+            return allTemplates;
+        }
+
+        List<DisplayTemplate> filteredTemplates = new ArrayList<DisplayTemplate>();
+        for (DisplayTemplate template : allTemplates) {
+            if (!isExcluded(excludes, template)) {
+                filteredTemplates.add(template);
+            }
+        }
+        return filteredTemplates;
+    }
+
+    private boolean isExcluded(Map<String, String> excludes, DisplayTemplate template) {
+        Enumeration<String> propertyNames = (Enumeration<String>) template.getProperties().propertyNames();
+        for (String property : Collections.list(propertyNames)) {
+            if (excludes.containsKey(property) && template.getProperties().getProperty(property).equals(excludes.get(property))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Map<String, String> parseFilter(String includeFilter) {
+        Map<String, String> filters = new HashMap<String, String>();
+        if (StringUtils.isNotEmpty(includeFilter)) {
+            String[] filterArr = includeFilter.split(FILTER_SEPARATOR);
+            if (filterArr != null) {
+                for (String filter : filterArr) {
+                    String[] filterNameVal = filter.split("=");
+                    if (filterNameVal != null && filterNameVal.length == 2) {
+                        filters.put(filterNameVal[0], filterNameVal[1]);
+                    }
+                }
+            }
+        }
+        return filters;
     }
 
     public void setTemplateConfigurationCache(TemplateConfigurationCache templateConfigurationCache) {
