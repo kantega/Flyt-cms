79f5499ae8456ce1fb8cb4a812eefe9bc612addb
Jørund Leknes <Jorund.Leknes@kantega.no>
2011-08-29 15:31:29 +0000

    AP-1262
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@3071 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/admin/content/ajax/ApproveOrRejectAction.java
+++ modules/core/src/java/no/kantega/publishing/admin/content/ajax/ApproveOrRejectAction.java
@@ -16,19 +16,29 @@
 
 package no.kantega.publishing.admin.content.ajax;
 
-import org.springframework.web.servlet.mvc.Controller;
+import no.kantega.commons.client.util.RequestParameters;
+import no.kantega.commons.exception.ConfigurationException;
+import no.kantega.commons.util.LocaleLabels;
+import no.kantega.publishing.admin.AdminSessionAttributes;
+import no.kantega.publishing.common.Aksess;
+import no.kantega.publishing.common.data.Content;
+import no.kantega.publishing.common.data.ContentIdentifier;
+import no.kantega.publishing.common.data.enums.ContentStatus;
+import no.kantega.publishing.common.service.ContentManagementService;
+import no.kantega.publishing.modules.mailsender.MailSender;
+import no.kantega.publishing.security.data.User;
+import no.kantega.publishing.security.realm.SecurityRealm;
+import no.kantega.publishing.security.realm.SecurityRealmFactory;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+import org.springframework.web.util.HtmlUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
-import no.kantega.publishing.common.data.ContentIdentifier;
-import no.kantega.publishing.common.data.Content;
-import no.kantega.publishing.common.data.enums.ContentStatus;
-import no.kantega.publishing.common.service.ContentManagementService;
-import no.kantega.publishing.admin.AdminSessionAttributes;
-import no.kantega.commons.client.util.RequestParameters;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -51,12 +61,53 @@ public class ApproveOrRejectAction implements Controller {
         if (approve || reject) {
             int status = approve ? ContentStatus.PUBLISHED : ContentStatus.REJECTED;
             Content content = aksessService.setContentStatus(cid, status, note);
-            Content currentNavigateContent = (Content)session.getAttribute(AdminSessionAttributes.CURRENT_NAVIGATE_CONTENT);
+            Content currentNavigateContent = (Content) session.getAttribute(AdminSessionAttributes.CURRENT_NAVIGATE_CONTENT);
             if (currentNavigateContent != null && currentNavigateContent.getId() == content.getId()) {
                 session.setAttribute(AdminSessionAttributes.CURRENT_NAVIGATE_CONTENT, content);
+            }
+            if (status == ContentStatus.REJECTED) {
+                sendRejectMail(content, note);
             }
         }
 
         return null;
+    }
+
+    private void sendRejectMail(Content content, String note) throws ConfigurationException {
+        String from = Aksess.getConfiguration().getString("mail.from", "noreply");
+        String to = getEmailForUser(content.getModifiedBy());
+
+        String contenturl = Aksess.getApplicationUrl() + "/admin/publish/Navigate.action?thisId=" + content.getAssociation().getAssociationId();
+        String title = content.getTitle();
+        if (StringUtils.isEmpty(title)) {
+            title = LocaleLabels.getLabel("aksess.reject.notitle", Aksess.getDefaultLocale());
+        }
+
+        Map<String, String> param = new HashMap<String, String>();
+        param.put("contenturl", contenturl);
+        param.put("title", title);
+        param.put("note", HtmlUtils.htmlEscape(note));
+        param.put("editor", Aksess.getConfiguration().getString("mail.editor", "noreply"));
+
+        String messageBody = MailSender.createStringFromVelocityTemplate("contentrejected.vm", param);
+        String subject = LocaleLabels.getLabel("aksess.reject.mailsubject", Aksess.getDefaultLocale());
+        if (StringUtils.isNotBlank(to)) {
+            MailSender.send(from, to, subject, messageBody);
+        }
+    }
+
+    // Null-safe lookup of email for userid.
+    private String getEmailForUser(String userid) {
+        String email = "";
+        if (StringUtils.isNotBlank(userid)) {
+            SecurityRealm realm = SecurityRealmFactory.getInstance();
+            if (realm != null) {
+                User user = realm.lookupUser(userid);
+                if (user != null) {
+                    email = user.getEmail();
+                }
+            }
+        }
+        return email;
     }
 }
