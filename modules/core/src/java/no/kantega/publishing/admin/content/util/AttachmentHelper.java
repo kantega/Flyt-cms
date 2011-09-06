ff3b83bf76f6f597958598bc410836522eed1208
Terje Røstum <Terje.Rostum@kantega.no>
2011-09-06 07:39:03 +0000

    AP-1270: Extract functionality for saving attachment
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@3084 fd808399-8219-4f14-9d4c-37719d9ec93d

---
+++ modules/core/src/java/no/kantega/publishing/admin/content/util/AttachmentHelper.java
@@ -0,0 +1,71 @@
+package no.kantega.publishing.admin.content.util;
+
+/*
+ * Copyright 2009 Kantega AS
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import no.kantega.publishing.common.ao.AttachmentAO;
+import no.kantega.publishing.common.data.Attachment;
+import no.kantega.publishing.common.data.Content;
+import no.kantega.publishing.common.data.attributes.FileAttribute;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public class AttachmentHelper {
+    public static void saveFileAsContentAttachment(Content content, FileAttribute fileAttribute, MultipartFile importFile) throws IOException {
+        int oldId = -1;
+        try {
+            oldId = Integer.parseInt(fileAttribute.getValue());
+        } catch (NumberFormatException e) {
+
+        }
+
+        // Brukeren har lastet opp en fil, legg inn basen
+        Attachment attachment = new Attachment();
+        attachment.setContentId(content.getId());
+        attachment.setLanguage(content.getLanguage());
+        if (!fileAttribute.isKeepOldVersions() && oldId != -1) {
+            // Delete old version
+            attachment.setId(oldId);
+        }
+
+        byte[] data = importFile.getBytes();
+
+        String filename = getFilename(importFile);
+
+        attachment.setFilename(filename);
+        attachment.setData(data);
+        attachment.setSize(data.length);
+
+        fileAttribute.setValue("" + AttachmentAO.setAttachment(attachment));
+
+        attachment.setData(null);
+
+        ensureContentIdIsUpdatedWhenContentIsPublished(content, attachment);
+    }
+
+    private static void ensureContentIdIsUpdatedWhenContentIsPublished(Content content, Attachment attachment) {
+        content.addAttachment(attachment);
+    }
+
+    private static String getFilename(MultipartFile importFile) {
+        String filename = importFile.getOriginalFilename();
+        if (filename.length() > 255) {
+            filename = filename.substring(filename.length() - 255, filename.length());
+        }
+        return filename;
+    }
+}
