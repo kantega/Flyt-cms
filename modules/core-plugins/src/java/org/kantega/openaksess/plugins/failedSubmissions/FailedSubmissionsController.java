ea56ba9e400018936a2f4e15e11b66a3a8bd73da
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2011-09-30 07:10:26 +0000

    HIST-525, Begrenser tidsrommet det skal vises feilede innsendinger fra. Konfigurerbart med failedsubmissions.interval=XX
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@3138 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core-plugins/src/java/org/kantega/openaksess/plugins/failedSubmissions/FailedSubmissionsController.java
+++ modules/core-plugins/src/java/org/kantega/openaksess/plugins/failedSubmissions/FailedSubmissionsController.java
@@ -1,5 +1,7 @@
 package org.kantega.openaksess.plugins.failedSubmissions;
 
+import no.kantega.commons.exception.ConfigurationException;
+import no.kantega.publishing.common.Aksess;
 import no.kantega.publishing.common.ao.EventLogAO;
 import no.kantega.publishing.common.data.EventLogEntry;
 import no.kantega.publishing.common.data.enums.Event;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Calendar;
 import java.util.List;
 
 @Controller
@@ -19,9 +22,17 @@ public class FailedSubmissionsController {
 
     @RequestMapping(method = RequestMethod.GET)
     public String getFailedEmailSubmissionEvents(@RequestParam(defaultValue = Event.FAILED_FORM_SUBMISSION, required = false) String eventName, Model model) throws Exception {
-        List<EventLogEntry> list = eventLogAO.createQuery().setEventName(eventName).list();
+        Calendar from = getFromDate();
+        List<EventLogEntry> list = eventLogAO.createQuery().setEventName(eventName).setFrom(from.getTime()).list();
         model.addAttribute("failedSubmissions", list);
         return "org/kantega/openaksess/plugins/failedSubmissions/view";
+    }
+
+    private Calendar getFromDate() throws ConfigurationException {
+        Calendar from = Calendar.getInstance();
+        int numberOfMinutesToShow = Aksess.getConfiguration().getInt("failedsubmissions.interval", 15);
+        from.add(Calendar.MINUTE, -numberOfMinutesToShow);
+        return from;
     }
 
     @Required
