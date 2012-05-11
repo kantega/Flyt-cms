c6b4d7bbee72ed3e1e30f019da24162a2a178972
Anders Skar <Anders.Skar@kantega.no>
2012-05-11 10:37:02 +0000

    Uppercase field names
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@3489 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/webapp/src/webapp/WEB-INF/dbmigrate/oa/201203221500/all/addimporttmtopic.sql
+++ modules/webapp/src/webapp/WEB-INF/dbmigrate/oa/201203221500/all/addimporttmtopic.sql
@@ -1,3 +1,3 @@
-ALTER TABLE tmassociation ADD imported INTEGER;
-ALTER TABLE tmtopic ADD imported INTEGER;
-ALTER TABLE tmmaps ADD url VARCHAR(255);
+ALTER TABLE tmassociation ADD Imported INTEGER;
+ALTER TABLE tmtopic ADD Imported INTEGER;
+ALTER TABLE tmmaps ADD Url VARCHAR(255);
