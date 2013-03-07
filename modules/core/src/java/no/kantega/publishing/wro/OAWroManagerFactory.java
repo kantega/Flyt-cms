173fcbbb6ce0863e699193bb1b2b977ef7c4c613
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2013-03-07 18:34:50 +0000

    AP-1547 - Merge ehcache.xml
    git-svn-id: https://opensource.kantega.no/svn/openaksess/branches/openaksess-solr@4120 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/wro/OAWroManagerFactory.java
+++ modules/core/src/java/no/kantega/publishing/wro/OAWroManagerFactory.java
@@ -1,26 +1,15 @@
 package no.kantega.publishing.wro;
 
 import no.kantega.publishing.spring.RootContext;
+import no.kantega.publishing.wro.xmlmerge.XmlMerger;
 import org.springframework.web.context.WebApplicationContext;
-import org.w3c.dom.Document;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
 import ro.isdc.wro.manager.factory.ConfigurableWroManagerFactory;
 import ro.isdc.wro.model.factory.WroModelFactory;
 import ro.isdc.wro.model.factory.XmlModelFactory;
 
 import javax.servlet.ServletContext;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.*;
-import java.net.MalformedURLException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class OAWroManagerFactory extends ConfigurableWroManagerFactory {
 
@@ -37,65 +26,9 @@ public class OAWroManagerFactory extends ConfigurableWroManagerFactory {
                 WebApplicationContext wac = (WebApplicationContext) RootContext.getInstance();
                 ServletContext servletContext = wac.getServletContext();
 
-                try {
-                    InputStream oaResourceStream = servletContext.getResourceAsStream(OA_XML_CONFIG_FILE);
-
-                    if(oaResourceStream == null) {
-                        throw new IllegalStateException("Could not find WRO config file at " + OA_XML_CONFIG_FILE);
-                    }
-
-                    InputStream projectResourceStream = servletContext.getResourceAsStream(PROJECT_XML_CONFIG_FILE);
-
-                    boolean onlyOA = projectResourceStream == null;
-                    if(onlyOA) {
-                        return oaResourceStream;
-                    } else {
-                        return merge(oaResourceStream, projectResourceStream);
-                    }
-                } catch (MalformedURLException e) {
-                    throw new RuntimeException(e);
-                } catch (ParserConfigurationException e) {
-                    throw new RuntimeException(e);
-                } catch (SAXException e) {
-                    throw new RuntimeException(e);
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                } catch (TransformerException e) {
-                    throw new RuntimeException(e);
-                }
+                return XmlMerger.merge(OA_XML_CONFIG_FILE, PROJECT_XML_CONFIG_FILE, servletContext);
             }
         };
-    }
-
-    public InputStream merge(InputStream oaResourceStream, InputStream projectResourceStream) throws ParserConfigurationException, IOException, SAXException, TransformerException {
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-
-        DocumentBuilder builder = dbf.newDocumentBuilder();
-
-        Document oaDoc = builder.parse(oaResourceStream);
-
-        Document projectDoc = builder.parse(projectResourceStream);
-
-        return serialize(merge(oaDoc, projectDoc));
-    }
-
-    private InputStream serialize(Document merge) throws TransformerException, UnsupportedEncodingException {
-        Transformer fac = TransformerFactory.newInstance().newTransformer();
-
-        StringWriter writer = new StringWriter();
-        fac.transform(new DOMSource(merge), new StreamResult(writer));
-        return new ByteArrayInputStream(writer.toString().getBytes("utf-8"));
-    }
-
-    private Document merge(Document oaDoc, Document projectDoc) {
-        NodeList children = projectDoc.getDocumentElement().getChildNodes();
-
-        for(int i = 0; i < children.getLength(); i++) {
-            Node importedNode = oaDoc.importNode(children.item(i), true);
-            oaDoc.getDocumentElement().appendChild(importedNode);
-        }
-
-        return oaDoc;
     }
 
 }
