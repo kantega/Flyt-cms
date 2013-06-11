38dba6fa1aad9164284ae7a6240962f8aa18b46d
Anders Skar <Anders.Skar@kantega.no>
2013-06-11 10:35:21 +0000

    AP-1596
    git-svn-id: https://opensource.kantega.no/svn/openaksess/branches/openaksess-7.4.x@4416 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/admin/content/InputScreenRenderer.java
+++ modules/core/src/java/no/kantega/publishing/admin/content/InputScreenRenderer.java
@@ -147,7 +147,7 @@ public class InputScreenRenderer {
 
             int numberOfRows = repeaterAttribute.getNumberOfRows();
             for (int rowNo = 0; rowNo < numberOfRows; rowNo++) {
-                out.print("<div class=\"contentAttributeRepeaterRow data-row=\"" + rowNo + "\">\n");
+                out.print("<div class=\"contentAttributeRepeaterRow\">\n");
                 request.setAttribute("repeaterRowNo", rowNo);
                 pageContext.include("/admin/publish/attributes/repeater_row_start.jsp");
                 List<Attribute> attributes = repeaterAttribute.getRow(rowNo);
