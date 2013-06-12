a97e9d160f72fbe9c7d2801d226e90b779d23a05
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2013-06-12 12:23:00 +0000

    AP-1597 Wrong total attachments number
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@4427 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/OA-Search/src/java/no/kantega/openaksess/search/provider/IndexableAttachmentProvider.java
+++ modules/OA-Search/src/java/no/kantega/openaksess/search/provider/IndexableAttachmentProvider.java
@@ -21,7 +21,7 @@ import java.util.concurrent.BlockingQueue;
 
 @Component
 public class IndexableAttachmentProvider implements IndexableDocumentProvider {
-
+    private final String FROM_CLAUSE = "FROM attachments, content, associations WHERE attachments.ContentId = content.ContentId AND content.IsSearchable = 1 AND content.ContentId = associations.ContentId AND associations.IsDeleted = 0";
     private final Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired
@@ -52,7 +52,7 @@ public class IndexableAttachmentProvider implements IndexableDocumentProvider {
 
     private void provideAttachments(BlockingQueue<IndexableDocument> indexableDocumentQueue) {
         try (Connection connection = dataSource.getConnection()){
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT DISTINCT attachments.Id FROM attachments, content, associations WHERE attachments.ContentId = content.ContentId AND content.IsSearchable = 1 AND content.ContentId = associations.ContentId AND associations.IsDeleted = 0");
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT DISTINCT attachments.Id " + FROM_CLAUSE);
             ResultSet resultSet = preparedStatement.executeQuery();
             while (resultSet.next()){
                 int id = resultSet.getInt("Id");
@@ -91,6 +91,6 @@ public class IndexableAttachmentProvider implements IndexableDocumentProvider {
 
     private long getNumberOfDocuments() {
         JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        return jdbcTemplate.queryForInt("SELECT count(attachments.Id) FROM attachments, content, associations WHERE attachments.ContentId = content.ContentId AND content.IsSearchable = 1 AND content.ContentId = associations.ContentId AND associations.IsDeleted = 0");
+        return jdbcTemplate.queryForInt("SELECT count( distinct attachments.Id) " + FROM_CLAUSE);
     }
 }
