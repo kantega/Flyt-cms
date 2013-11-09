86500c5959aaf88113a7bb87e1024a05b1f45f3d
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2013-11-09 14:17:57 +0000

    AP-1657 - Support non-embedded Solr server
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@4898 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/solr-search/src/java/no/kantega/openaksess/search/solr/config/SolrConfiguration.java
+++ modules/solr-search/src/java/no/kantega/openaksess/search/solr/config/SolrConfiguration.java
@@ -2,6 +2,8 @@ package no.kantega.openaksess.search.solr.config;
 
 import org.apache.solr.client.solrj.SolrServer;
 import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
+import org.apache.solr.client.solrj.impl.CloudSolrServer;
+import org.apache.solr.client.solrj.impl.HttpSolrServer;
 import org.apache.solr.core.CoreContainer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +16,7 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 
 import static no.kantega.openaksess.search.solr.config.SolrConfigInitializer.initSolrConfigIfAbsent;
+import static org.apache.commons.lang.StringUtils.isNotBlank;
 
 @Configuration
 public class SolrConfiguration {
@@ -24,11 +27,23 @@ public class SolrConfiguration {
     @Value("${disableUpdateSolrHome:false}")
     private boolean disableUpdateSolrHome;
 
+    @Value("httpSolrServerUrl:")
+    private String httpSolrServerUrl;
+
+    @Value("httpSolrServerUrl:")
+    private String cloudSolrServer;
+
     @Bean(destroyMethod = "shutdown")
     public SolrServer getSolrServer() throws IOException, SAXException, ParserConfigurationException, URISyntaxException {
-        File solrConfigFile = initSolrConfigIfAbsent(solrHome, disableUpdateSolrHome);
-        CoreContainer container = CoreContainer.createAndLoad(solrHome.getAbsolutePath(), solrConfigFile);
+        if(isNotBlank(cloudSolrServer)){
+            return new CloudSolrServer(cloudSolrServer);
+        } else if(isNotBlank(httpSolrServerUrl)){
+            return new HttpSolrServer(httpSolrServerUrl);
+        } else {
+            File solrConfigFile = initSolrConfigIfAbsent(solrHome, disableUpdateSolrHome);
+            CoreContainer container = CoreContainer.createAndLoad(solrHome.getAbsolutePath(), solrConfigFile);
 
-        return new EmbeddedSolrServer(container, "oacore");
+            return new EmbeddedSolrServer(container, "oacore");
+        }
     }
 }
