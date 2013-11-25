fdf5aa22a9bcfc25c87266d40c85d18748170acf
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2013-11-25 15:58:16 +0000

    LocationUtil.locationWithoutTrailingSlash returned a totaly wrong value.
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@4954 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/OA-Search/src/java/no/kantega/openaksess/search/provider/transformer/LocationUtil.java
+++ modules/OA-Search/src/java/no/kantega/openaksess/search/provider/transformer/LocationUtil.java
@@ -1,16 +1,14 @@
 package no.kantega.openaksess.search.provider.transformer;
 
 import no.kantega.publishing.common.data.Association;
-import org.apache.commons.lang.StringUtils;
 
 public class LocationUtil {
     public static String locationWithoutTrailingSlash(Association association) {
-        String cleanedPath = association.getPath();
-        if(cleanedPath.contains("/")){
-            cleanedPath = "/" + association.getSiteId();
-        } else if(cleanedPath.endsWith("/") && cleanedPath.length() > 2){
-            cleanedPath = StringUtils.substringBeforeLast(cleanedPath, "/");
-        }
-        return cleanedPath;
+        String pathWithSite = "/" + association.getSiteId() + association.getPath();
+
+
+        return pathWithSite.endsWith("/") ?
+                pathWithSite.substring(0, pathWithSite.length() - 1)
+                : pathWithSite;
     }
 }
