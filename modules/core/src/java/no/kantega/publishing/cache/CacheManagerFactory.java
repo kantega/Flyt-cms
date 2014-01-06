a7570c9333d7b5f704726481b4a51811d195e9f4
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2014-01-06 20:24:05 +0000

    AP-1719  Set unique cache dir and cache name
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@5032 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/cache/CacheManagerFactory.java
+++ modules/core/src/java/no/kantega/publishing/cache/CacheManagerFactory.java
@@ -1,12 +1,17 @@
 package no.kantega.publishing.cache;
 
 import net.sf.ehcache.CacheException;
+import net.sf.ehcache.CacheManager;
+import net.sf.ehcache.config.Configuration;
+import net.sf.ehcache.config.ConfigurationFactory;
+import net.sf.ehcache.config.DiskStoreConfiguration;
 import net.sf.ehcache.management.ManagementService;
 import no.kantega.publishing.wro.xmlmerge.XmlMerger;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
-import org.springframework.core.io.InputStreamResource;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.FactoryBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.web.context.ServletContextAware;
 
 import javax.management.MBeanServer;
@@ -15,12 +20,19 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.lang.management.ManagementFactory;
 
-public class CacheManagerFactory extends EhCacheManagerFactoryBean implements ServletContextAware {
+/**
+ * Based on EhCacheManagerFactoryBean
+ * @see org.springframework.cache.ehcache.EhCacheManagerFactoryBean
+ */
+public class CacheManagerFactory implements ServletContextAware, FactoryBean<CacheManager>, InitializingBean, DisposableBean {
+
+    private static final Logger log = LoggerFactory.getLogger(CacheManagerFactory.class);
 
     private ServletContext servletContext;
 
+    private CacheManager cacheManager;
+
     public enum CacheNames {AliasCache, ContentCache, ContentListCache, SiteMapCache, XmlCache, FacetLabelCache, ContentUrlCache, ContentIdentifierCache, ImageCache, UserCache}
-    private static final Logger log = LoggerFactory.getLogger(CacheManagerFactory.class);
 
     private static final String OA_XML_CONFIG_FILE = "/WEB-INF/ehcache-oa.xml";
     private static final String PROJECT_XML_CONFIG_FILE = "/WEB-INF/ehcache-project.xml";
@@ -28,9 +40,13 @@ public class CacheManagerFactory extends EhCacheManagerFactoryBean implements Se
     @Override
     public void afterPropertiesSet() throws IOException, CacheException {
         InputStream is = XmlMerger.merge(OA_XML_CONFIG_FILE, PROJECT_XML_CONFIG_FILE, servletContext);
-        setConfigLocation(new InputStreamResource(is));
 
-        super.afterPropertiesSet();
+        Configuration configuration = ConfigurationFactory.parseConfiguration(is);
+        String path = System.getProperty("java.io.tmpdir") + servletContext.getContextPath();
+        configuration.addDiskStore(new DiskStoreConfiguration().path(path));
+        configuration.setName(configuration.getName() + servletContext.getContextPath());
+        cacheManager = new CacheManager(configuration);
+
         createMBean();
     }
 
@@ -44,4 +60,22 @@ public class CacheManagerFactory extends EhCacheManagerFactoryBean implements Se
     public void setServletContext(ServletContext servletContext) {
         this.servletContext = servletContext;
     }
+
+    public CacheManager getObject() {
+        return this.cacheManager;
+    }
+
+    public Class<? extends CacheManager> getObjectType() {
+        return (this.cacheManager != null ? this.cacheManager.getClass() : CacheManager.class);
+    }
+
+    public boolean isSingleton() {
+        return true;
+    }
+
+    public void destroy() {
+        log.info("Shutting down EhCache CacheManager");
+        this.cacheManager.shutdown();
+    }
+
 }
