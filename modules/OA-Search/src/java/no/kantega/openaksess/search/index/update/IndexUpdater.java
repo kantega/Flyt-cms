a3c7ab0b14fb1f8a16190e830b40388abed529b8
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2014-04-10 11:59:20 +0000

    AP-1794
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@5336 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/OA-Search/src/java/no/kantega/openaksess/search/index/update/IndexUpdater.java
+++ modules/OA-Search/src/java/no/kantega/openaksess/search/index/update/IndexUpdater.java
@@ -13,6 +13,8 @@ import no.kantega.publishing.content.api.ContentAO;
 import no.kantega.publishing.event.ContentEvent;
 import no.kantega.publishing.event.ContentEventListenerAdapter;
 import no.kantega.search.api.index.DocumentIndexer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -21,6 +23,8 @@ import java.util.List;
 
 @Component
 public class IndexUpdater extends ContentEventListenerAdapter {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private ContentAO contentAO;
 
@@ -41,31 +45,39 @@ public class IndexUpdater extends ContentEventListenerAdapter {
 
     @Override
     public void contentSaved(ContentEvent event) {
-        Content content = event.getContent();
-        if (content.isSearchable()) {
-            updateIndex(content);
-        } else {
-            contentDeleted(event);
+        try {
+            Content content = event.getContent();
+            if (content.isSearchable()) {
+                updateIndex(content);
+            } else {
+                contentDeleted(event);
+            }
+        } catch (Exception e) {
+            log.error("Error indexing saved content", e);
         }
     }
 
     @Override
     public void contentDeleted(ContentEvent event) {
-        Content content = event.getContent();
-        List<String> uids = new ArrayList<>();
-        List<Association> associations = AssociationAO.getAssociationsByContentId(event.getContent().getId());
-        if (associations != null) {
-            for (Association association : associations) {
-                uids.add(contentTransformer.generateUniqueID(content, association.getSiteId()));
+        try {
+            Content content = event.getContent();
+            List<String> uids = new ArrayList<>();
+            List<Association> associations = AssociationAO.getAssociationsByContentId(event.getContent().getId());
+            if (associations != null) {
+                for (Association association : associations) {
+                    uids.add(contentTransformer.generateUniqueID(content, association.getSiteId()));
+                }
             }
-        }
 
-        for (Attachment attachment : AttachmentAO.getAttachmentList(event.getContent().getContentIdentifier())) {
-            uids.add(attachmentTransformer.generateUniqueID(attachment));
-        }
+            for (Attachment attachment : AttachmentAO.getAttachmentList(event.getContent().getContentIdentifier())) {
+                uids.add(attachmentTransformer.generateUniqueID(attachment));
+            }
 
-        documentIndexer.deleteByUid(uids);
-        documentIndexer.commit();
+            documentIndexer.deleteByUid(uids);
+            documentIndexer.commit();
+        } catch (Exception e) {
+            log.error("Error deleting content from index", e);
+        }
     }
 
     @Override
@@ -93,29 +105,33 @@ public class IndexUpdater extends ContentEventListenerAdapter {
     @Override
     public void associationDeleted(ContentEvent event) {
         // TODO: Should remove children
-        List<String> uids = new ArrayList<>();
+        try {
+            List<String> uids = new ArrayList<>();
 
-        int siteId = event.getAssociation().getSiteId();
+            int siteId = event.getAssociation().getSiteId();
 
-        ContentIdentifier cid = ContentIdentifier.fromAssociationId(event.getAssociation().getAssociationId());
-        Content content = contentAO.getContent(cid, true);
-        if (content != null) {
-            boolean lastInstanceOnSite = true;
-            for (Association association : content.getAssociations()) {
-                if (association.getSiteId() == siteId) {
-                    // Don't remove from index if there still instances of this page is this site
-                    lastInstanceOnSite = false;
+            ContentIdentifier cid = ContentIdentifier.fromAssociationId(event.getAssociation().getAssociationId());
+            Content content = contentAO.getContent(cid, true);
+            if (content != null) {
+                boolean lastInstanceOnSite = true;
+                for (Association association : content.getAssociations()) {
+                    if (association.getSiteId() == siteId) {
+                        // Don't remove from index if there still instances of this page is this site
+                        lastInstanceOnSite = false;
+                    }
                 }
+                if (lastInstanceOnSite) {
+                    uids.add(contentTransformer.generateUniqueID(content, siteId));
+                }
+            } else {
+                // Content was deleted (all associations deleted), handled by contentDeleted method
             }
-            if (lastInstanceOnSite) {
-                uids.add(contentTransformer.generateUniqueID(content, siteId));
-            }
-        } else {
-            // Content was deleted (all associations deleted), handled by contentDeleted method
-        }
 
-        documentIndexer.deleteByUid(uids);
-        documentIndexer.commit();
+            documentIndexer.deleteByUid(uids);
+            documentIndexer.commit();
+        } catch (Exception e) {
+            log.error("Error indexing deleted association", e);
+        }
     }
 
     @Override
@@ -124,9 +140,13 @@ public class IndexUpdater extends ContentEventListenerAdapter {
     }
 
     public void attachmentUpdated(ContentEvent event) {
-        Attachment attachment = event.getAttachment();
-        if (attachment.getContentId() != -1) {
-            documentIndexer.indexDocumentAndCommit(attachmentTransformer.transform(attachment));
+        try {
+            Attachment attachment = event.getAttachment();
+            if (attachment.getContentId() != -1) {
+                documentIndexer.indexDocumentAndCommit(attachmentTransformer.transform(attachment));
+            }
+        } catch (Exception e) {
+            log.error("Error indexing updated attachment", e);
         }
     }
 
