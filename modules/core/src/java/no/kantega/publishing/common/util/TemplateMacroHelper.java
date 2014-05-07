c746bc530b106dae70aad3a4209fc5ffc4776a31
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2014-05-07 12:26:46 +0000

    AP-969 compile site, device and language patterns used for macros up front.
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@5374 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/common/util/TemplateMacroHelper.java
+++ modules/core/src/java/no/kantega/publishing/common/util/TemplateMacroHelper.java
@@ -4,7 +4,12 @@ import no.kantega.publishing.api.content.Language;
 import no.kantega.publishing.api.model.Site;
 import no.kantega.publishing.client.device.DeviceCategory;
 
+import java.util.regex.Pattern;
+
 public class TemplateMacroHelper {
+    private static final Pattern SITE_PATTERN = Pattern.compile("\\$SITE");
+    private static final Pattern DEVICE_PATTERN = Pattern.compile("\\$DEVICE");
+    private static final Pattern LANGUAGE_PATTERN = Pattern.compile("\\$LANGUAGE");
 
     public static boolean containsMacro(String templateFilename) {
         return templateFilename.contains("$SITE") || templateFilename.contains("$DEVICE") || templateFilename.contains("$LANGUAGE");
@@ -18,15 +23,15 @@ public class TemplateMacroHelper {
 
         if (templateFilename.contains("$SITE")) {
             String alias = site.getAlias();
-            templateFilename = templateFilename.replaceAll("\\$SITE", alias.substring(0, alias.length() - 1));
+            templateFilename = SITE_PATTERN.matcher(templateFilename).replaceAll(alias.substring(0, alias.length() - 1));
         }
 
         if (templateFilename.contains("$DEVICE")) {
-            templateFilename = templateFilename.replaceAll("\\$DEVICE", deviceCategory.toString());
+            templateFilename = DEVICE_PATTERN.matcher(templateFilename).replaceAll(deviceCategory.toString());
         }
 
         if (language != null && templateFilename.contains("$LANGUAGE")) {
-            templateFilename = templateFilename.replaceAll("\\$LANGUAGE", Language.getLanguageAsISOCode(language));
+            templateFilename = LANGUAGE_PATTERN.matcher(templateFilename).replaceAll(Language.getLanguageAsISOCode(language));
         }
 
         return templateFilename;
