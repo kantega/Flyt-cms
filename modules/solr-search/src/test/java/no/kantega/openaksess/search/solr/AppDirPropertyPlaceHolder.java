6cf6c824d63934ced8e41030758ff43878f24a24
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2014-05-12 17:43:28 +0000

    AP-969
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@5430 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/solr-search/src/test/java/no/kantega/openaksess/search/solr/AppDirPropertyPlaceHolder.java
+++ modules/solr-search/src/test/java/no/kantega/openaksess/search/solr/AppDirPropertyPlaceHolder.java
@@ -3,18 +3,17 @@ package no.kantega.openaksess.search.solr;
 import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.stereotype.Component;
 
-import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.Properties;
 
 @Component
 public class AppDirPropertyPlaceHolder extends PropertyPlaceholderConfigurer {
-    public static File appDir = new File(System.getProperty("java.io.tmpdir"), "aksesstest");
 
     @Override
     protected Properties mergeProperties() throws IOException {
         Properties properties = super.mergeProperties();
-        properties.put("appDir", appDir.getAbsolutePath());
+        properties.put("appDir", Files.createTempDirectory("aksessSolrTest").toFile().getAbsolutePath());
         return properties;
     }
 }
--- modules/solr-search/src/test/java/no/kantega/openaksess/search/solr/IndexDirDeleter.java
+++ modules/solr-search/src/test/java/no/kantega/openaksess/search/solr/IndexDirDeleter.java
@@ -1,15 +1,21 @@
 package no.kantega.openaksess.search.solr;
 
 import org.apache.commons.io.FileUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PreDestroy;
+import java.io.File;
 import java.io.IOException;
 
 @Component
 public class IndexDirDeleter {
+
+    @Value("${appDir}")
+    private File appDir;
+
     @PreDestroy
     public void deleteAppDir() throws IOException {
-        FileUtils.deleteDirectory(AppDirPropertyPlaceHolder.appDir);
+        FileUtils.deleteDirectory(appDir);
     }
 }
