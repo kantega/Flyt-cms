9791be5c9541dde53a9ada9a733e4eef4ec6e501
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2014-05-22 08:42:32 +0000

    AP-969 Using SecuritySession.getInstance(request) in SecuritySessionConfiguration
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@5466 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/admin/config/SecuritySessionConfiguration.java
+++ modules/core/src/java/no/kantega/publishing/admin/config/SecuritySessionConfiguration.java
@@ -1,7 +1,6 @@
 package no.kantega.publishing.admin.config;
 
 import no.kantega.publishing.security.SecuritySession;
-import no.kantega.publishing.security.realm.SecurityRealm;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -9,40 +8,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
 
 @Configuration
 public class SecuritySessionConfiguration {
     private final Logger log = LoggerFactory.getLogger(SecuritySessionConfiguration.class);
 
-    private SecurityRealm securityRealm;
-
     @Bean
     @Scope("request")
     public SecuritySession securitySession(HttpServletRequest request){
-        SecuritySession unauthenticatedInstance = createNewSecuritySession();
-        SecuritySession securitySession;
-        HttpSession session = request.getSession(false);
-        if(session == null){
-            log.debug("Returning unauthenticatedInstance");
-            securitySession = unauthenticatedInstance;
-        } else {
-            securitySession = (SecuritySession) session.getAttribute("aksess.securitySession");
-            if (securitySession == null){
-                log.debug("Returning unauthenticatedInstance");
-                securitySession = unauthenticatedInstance;
-            } else {
-                log.debug("Returning existing aksess.securitySession");
-            }
-        }
-        return securitySession;
-    }
-
-    private SecuritySession createNewSecuritySession() {
-        return new SecuritySession(securityRealm);
-    }
-
-    public void setSecurityRealm(SecurityRealm securityRealm) {
-        this.securityRealm = securityRealm;
+        log.debug("Getting SecuritySession");
+        return SecuritySession.getInstance(request);
     }
 }
