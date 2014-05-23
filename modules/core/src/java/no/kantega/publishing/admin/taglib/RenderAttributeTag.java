a43040adc1f5f093969f5c95c4c1f29bd641ad2a
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2014-05-23 08:34:57 +0000

    AP-969
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@5472 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/admin/taglib/RenderAttributeTag.java
+++ modules/core/src/java/no/kantega/publishing/admin/taglib/RenderAttributeTag.java
@@ -2,7 +2,6 @@ package no.kantega.publishing.admin.taglib;
 
 import no.kantega.commons.client.util.ValidationError;
 import no.kantega.commons.client.util.ValidationErrors;
-import no.kantega.commons.exception.InvalidFileException;
 import no.kantega.commons.exception.SystemException;
 import no.kantega.commons.util.LocaleLabels;
 import no.kantega.publishing.admin.AdminSessionAttributes;
@@ -11,9 +10,8 @@ import no.kantega.publishing.common.Aksess;
 import no.kantega.publishing.common.data.Content;
 import no.kantega.publishing.common.data.attributes.Attribute;
 import no.kantega.publishing.common.data.enums.AttributeDataType;
-import no.kantega.publishing.common.exception.InvalidTemplateException;
 import no.kantega.publishing.common.factory.AttributeFactory;
-import no.kantega.publishing.spring.RootContext;
+import org.springframework.web.context.support.WebApplicationContextUtils;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.jsp.JspException;
@@ -33,6 +31,7 @@ public class RenderAttributeTag extends TagSupport {
     private String helpText;
     private int maxLength = -1;
     private Attribute attribute;
+    private AttributeFactory attributeFactory;
 
     public void setType(String type) {
         this.type = type;
@@ -67,81 +66,74 @@ public class RenderAttributeTag extends TagSupport {
     }
 
     public int doStartTag() throws JspException {
-
+        setAttributeFactoryIfNull();
         HttpServletRequest request = (HttpServletRequest)pageContext.getRequest();
-            Content content = (Content)request.getSession(true).getAttribute(AdminSessionAttributes.CURRENT_EDIT_CONTENT);
+        Content content = (Content)request.getSession(true).getAttribute(AdminSessionAttributes.CURRENT_EDIT_CONTENT);
 
-            try {
-                if (attribute == null) {
-                    AttributeFactory attributeFactory = (AttributeFactory) RootContext.getInstance().getBean("aksessAttributeFactory");
-                    attribute = attributeFactory.newAttribute(type);
-                }
-                if (value != null && value.length() > 0) {
-                    attribute.setValue(value);
-                }
-                attribute.setName(name);
-                if (titlekey != null) {
-                    title = LocaleLabels.getLabel(titlekey, Aksess.getDefaultAdminLocale());
-                }
-                if (title != null) {
-                    attribute.setTitle(title);
-                }
-                if (helpText != null) {
-                    attribute.setHelpText(helpText);
-                }
-                if (maxLength != -1) {
-                    attribute.setMaxLength(maxLength);
-                }
+        try {
+            if (attribute == null) {
+                attribute = attributeFactory.newAttribute(type);
+            }
+            if (value != null && value.length() > 0) {
+                attribute.setValue(value);
+            }
+            attribute.setName(name);
+            if (titlekey != null) {
+                title = LocaleLabels.getLabel(titlekey, Aksess.getDefaultAdminLocale());
+            }
+            if (title != null) {
+                attribute.setTitle(title);
+            }
+            if (helpText != null) {
+                attribute.setHelpText(helpText);
+            }
+            if (maxLength != -1) {
+                attribute.setMaxLength(maxLength);
+            }
 
-                Map<String, List<ValidationError>> fieldErrors = new HashMap<String, List<ValidationError>>();
-                ValidationErrors errors = (ValidationErrors)request.getAttribute("errors");
-                if (errors != null) {
-                    for (ValidationError error : errors.getErrors()) {
-                        if (error.getField() != null && error.getField().length() > 0) {
-                            List<ValidationError> errorsForField = fieldErrors.get(error.getField());
-                            if (errorsForField == null) {
-                                errorsForField = new ArrayList<ValidationError>();
-                                fieldErrors.put(error.getField(), errorsForField);
-                            }
-                            errorsForField.add(error);
+            Map<String, List<ValidationError>> fieldErrors = new HashMap<>();
+            ValidationErrors errors = (ValidationErrors)request.getAttribute("errors");
+            if (errors != null) {
+                for (ValidationError error : errors.getErrors()) {
+                    if (error.getField() != null && error.getField().length() > 0) {
+                        List<ValidationError> errorsForField = fieldErrors.get(error.getField());
+                        if (errorsForField == null) {
+                            errorsForField = new ArrayList<>();
+                            fieldErrors.put(error.getField(), errorsForField);
                         }
+                        errorsForField.add(error);
                     }
                 }
-
-                InputScreenRenderer renderer = new InputScreenRenderer(pageContext, content, AttributeDataType.CONTENT_DATA);
-                renderer.renderNormalAttribute(pageContext.getOut(), request, fieldErrors, attribute);
-
-            } catch (IOException e) {
-                throw new JspException(e);
-            } catch (SystemException e) {
-                throw new JspException(e);
-            } catch (ClassNotFoundException e) {
-                throw new JspException(e);
-            } catch (InstantiationException e) {
-                throw new JspException(e);
-            } catch (IllegalAccessException e) {
-                throw new JspException(e);
-            } catch (InvalidFileException e) {
-                throw new JspException(e);
-            } catch (InvalidTemplateException e) {
-                throw new JspException(e);
-            } finally {
-                value = null;
-                helpText = null;
-                title = null;
-                name = null;
-                type = null;
-                titlekey = null;
-                maxLength = -1;
-                attribute = null;
             }
+
+            InputScreenRenderer renderer = new InputScreenRenderer(pageContext, content, AttributeDataType.CONTENT_DATA);
+            renderer.renderNormalAttribute(pageContext.getOut(), request, fieldErrors, attribute);
+
+        } catch (IOException | SystemException | ClassNotFoundException | InstantiationException | IllegalAccessException e) {
+            throw new JspException(e);
+        } finally {
+            value = null;
+            helpText = null;
+            title = null;
+            name = null;
+            type = null;
+            titlekey = null;
+            maxLength = -1;
+            attribute = null;
+        }
 
 
         return SKIP_BODY;
     }
 
+    private void setAttributeFactoryIfNull() {
+        if (attributeFactory == null){
+            attributeFactory = WebApplicationContextUtils.getRequiredWebApplicationContext(pageContext.getServletContext()).getBean("aksessAttributeFactory", AttributeFactory.class);
+        }
+    }
+
     public int doEndTag() throws JspException {
-         return EVAL_PAGE;
+        return EVAL_PAGE;
     }
 
 
