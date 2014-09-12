0d7e220045a7a329e5e90f2bb76cf0c03b8fc1b1
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2014-09-12 14:52:40 +0000

    AP-1866
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@5691 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/common/data/ServletResourceLoader.java
+++ modules/core/src/java/no/kantega/publishing/common/data/ServletResourceLoader.java
@@ -49,6 +49,11 @@ public class ServletResourceLoader extends DefaultResourceLoader implements Reso
         }
     }
 
+    @Override
+    public Resource getResource(String location) {
+        return getResourceByPath(location);
+    }
+
     public void setPrefix(String prefix) {
         this.prefix = prefix;
     }
