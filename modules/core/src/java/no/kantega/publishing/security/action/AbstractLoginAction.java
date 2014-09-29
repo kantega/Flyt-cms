1aba26846bcc7b6737a1f83cc46c6676b4a47b4b
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2014-09-29 17:58:18 +0000

    AP-969, index PasswordManagers on startup such that getPasswordManager is just a Map.get(domain)
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@5721 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/security/action/AbstractLoginAction.java
+++ modules/core/src/java/no/kantega/publishing/security/action/AbstractLoginAction.java
@@ -1,49 +1,41 @@
 package no.kantega.publishing.security.action;
 
+import com.google.common.collect.Maps;
 import no.kantega.publishing.security.realm.SecurityRealm;
 import no.kantega.publishing.security.realm.SecurityRealmFactory;
-import no.kantega.publishing.spring.RootContext;
 import no.kantega.security.api.identity.DefaultIdentity;
 import no.kantega.security.api.identity.Identity;
 import no.kantega.security.api.password.PasswordManager;
 import no.kantega.security.api.password.ResetPasswordTokenManager;
 import no.kantega.security.api.profile.ProfileManager;
-import org.springframework.context.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
+import static java.util.Collections.emptyMap;
 import static org.apache.http.util.TextUtils.isBlank;
 
 
 public abstract class AbstractLoginAction implements Controller {
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     private String loginLayout;
 
+    private Map<String, PasswordManager> passwordManagers;
+    private ResetPasswordTokenManager resetPasswordTokenManager;
+
     protected PasswordManager getPasswordManager(String domain) {
-        PasswordManager passwordManager = null;
-        ApplicationContext context = RootContext.getInstance();
-        Map passwordManagers = context.getBeansOfType(PasswordManager.class);
-        if (passwordManagers != null) {
-            for (Object o : passwordManagers.values()) {
-                passwordManager = (PasswordManager) o;
-                if (passwordManager.getDomain().equalsIgnoreCase(domain)) {
-                    break;
-                }
-            }
-        }
-        return passwordManager;
+        Objects.requireNonNull(domain, "Domain must be non-null!");
+        return passwordManagers.get(domain.toLowerCase());
     }
 
     protected ResetPasswordTokenManager getResetPasswordTokenManager() {
-        ResetPasswordTokenManager resetPasswordTokenManager = null;
-        ApplicationContext context = RootContext.getInstance();
-        Map resetPasswordTokenManagerers = context.getBeansOfType(ResetPasswordTokenManager.class);
-        if (resetPasswordTokenManagerers != null) {
-            for (Object o : resetPasswordTokenManagerers.values()) {
-                resetPasswordTokenManager = (ResetPasswordTokenManager) o;
-            }
-        }
         return resetPasswordTokenManager;
     }
 
@@ -71,5 +63,24 @@ public abstract class AbstractLoginAction implements Controller {
 
     public void setLoginLayout(String loginLayout) {
         this.loginLayout = loginLayout;
+    }
+
+    @Autowired(required = false)
+    public void setPasswordManagers(List<PasswordManager> passwordManagers) {
+        log.info("Setting PasswordManagers: " + passwordManagers);
+        if(passwordManagers == null){
+            this.passwordManagers = emptyMap();
+        } else {
+            this.passwordManagers = Maps.newHashMapWithExpectedSize(passwordManagers.size());
+            for (PasswordManager passwordManager : passwordManagers) {
+                this.passwordManagers.put(passwordManager.getDomain().toLowerCase(), passwordManager);
+            }
+        }
+    }
+
+    @Autowired(required = false)
+    public void setResetPasswordTokenManager(ResetPasswordTokenManager resetPasswordTokenManager) {
+        log.info("Setting ResetPasswordTokenManager: " + resetPasswordTokenManager);
+        this.resetPasswordTokenManager = resetPasswordTokenManager;
     }
 }
