38c011db112ff3b4e0373fb64bfc8c0066aeeb40
Marvin Lillehaug <Marvin.Lillehaug@kantega.no>
2014-10-08 07:16:26 +0000

    AP-969 "" + id -> valueOf(id)
    git-svn-id: https://opensource.kantega.no/svn/openaksess/trunk@5752 fd808399-8219-4f14-9d4c-37719d9ec93d

--- modules/core/src/java/no/kantega/publishing/admin/content/behaviours/attributes/PersistMediaAttributeBehaviour.java
+++ modules/core/src/java/no/kantega/publishing/admin/content/behaviours/attributes/PersistMediaAttributeBehaviour.java
@@ -101,7 +101,7 @@ public class PersistMediaAttributeBehaviour implements PersistAttributeBehaviour
                     multimediaUploadHandler.updateMultimediaWithData(multimedia, importFile.getBytes(), filename, true);
                     multimedia.setOwnerPerson(content.getOwnerPerson());
                     int id = multimediaAO.setMultimedia(multimedia);
-                    mediaAttr.setValue("" + id);
+                    mediaAttr.setValue(String.valueOf(id));
                     mediaAttr.setImportFile(null);
                 }
             } catch (IllegalStateException e) {
