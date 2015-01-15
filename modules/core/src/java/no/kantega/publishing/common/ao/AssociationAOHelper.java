e4e189d61b1cdaae53fdb4c5f417e9b6853ba600
Marvin Bredal Lillehaug <marlil@kantega.no>
2015-01-15 09:27:52 +0100

    AP-969 Try with resources in AssociationAOHelper.java

--- modules/core/src/java/no/kantega/publishing/common/ao/AssociationAOHelper.java
+++ modules/core/src/java/no/kantega/publishing/common/ao/AssociationAOHelper.java
@@ -32,28 +32,26 @@ import java.util.List;
 public class AssociationAOHelper {
 
     public static void fixDefaultPostings() throws SystemException {
-        try (Connection c = dbConnectionFactory.getConnection()) {
-            List<Site> sites = RootContext.getInstance().getBean(SiteCache.class).getSites();
+        List<Site> sites = RootContext.getInstance().getBean(SiteCache.class).getSites();
+        // MySQL støtter ikke å oppdatere tabeller som er med i subqueries, derfor denne tungvinte måten å gjøre det på
+        String query = "SELECT min(uniqueid) from associations WHERE siteid = ? AND type = " + AssociationType.CROSS_POSTING + " AND (IsDeleted IS NULL OR IsDeleted = 0) AND contentid NOT IN " +
+                " (SELECT contentid from associations WHERE siteid = ? AND type = " + AssociationType.DEFAULT_POSTING_FOR_SITE + " AND (IsDeleted IS NULL OR IsDeleted = 0)) GROUP BY contentid";
+        String updateQuery = "UPDATE associations SET type = " + AssociationType.DEFAULT_POSTING_FOR_SITE + " WHERE uniqueid = ? AND (IsDeleted IS NULL OR IsDeleted = 0)";
 
-            // MySQL støtter ikke å oppdatere tabeller som er med i subqueries, derfor denne tungvinte måten å gjøre det på
-            String query = "SELECT min(uniqueid) from associations WHERE siteid = ? AND type = " + AssociationType.CROSS_POSTING + " AND (IsDeleted IS NULL OR IsDeleted = 0) AND contentid NOT IN " +
-                            " (SELECT contentid from associations WHERE siteid = ? AND type = " + AssociationType.DEFAULT_POSTING_FOR_SITE + " AND (IsDeleted IS NULL OR IsDeleted = 0)) GROUP BY contentid";
-            PreparedStatement st = c.prepareStatement(query);
-
-            String updateQuery = "UPDATE associations SET type = " + AssociationType.DEFAULT_POSTING_FOR_SITE + " WHERE uniqueid = ? AND (IsDeleted IS NULL OR IsDeleted = 0)";
-            PreparedStatement updateSt = c.prepareStatement(updateQuery);
+        try (Connection c = dbConnectionFactory.getConnection();
+             PreparedStatement st = c.prepareStatement(query);
+             PreparedStatement updateSt = c.prepareStatement(updateQuery)) {
 
             for (Site site : sites) {
                 st.setInt(1, site.getId());
                 st.setInt(2, site.getId());
-                ResultSet rs = st.executeQuery();
-                while (rs.next()) {
-                    int id = rs.getInt(1);
-                    updateSt.setInt(1, id);
-                    updateSt.executeUpdate();
+                try(ResultSet rs = st.executeQuery()) {
+                    while (rs.next()) {
+                        int id = rs.getInt(1);
+                        updateSt.setInt(1, id);
+                        updateSt.executeUpdate();
+                    }
                 }
-                rs.close();
-                rs = null;
             }
         } catch (SQLException e) {
             throw new SystemException("SQL Feil ved databasekall", e);
@@ -67,22 +65,21 @@ public class AssociationAOHelper {
             if (dbConnectionFactory.isMySQL()) {
                 // MySQL støtter ikke å slette tabeller som er med i subqueries, derfor denne tungvinte måten å gjøre det på
                 String query = "SELECT UniqueId FROM associations WHERE type = " + AssociationType.SHORTCUT + " AND AssociationId NOT IN (SELECT UniqueId FROM associations WHERE (IsDeleted IS NULL OR IsDeleted = 0)) AND (IsDeleted IS NULL OR IsDeleted = 0)";
-                PreparedStatement st = c.prepareStatement(query);
-
                 String updateQuery = "DELETE FROM associations WHERE UniqueId = ?";
-                PreparedStatement updateSt = c.prepareStatement(updateQuery);
+                try(PreparedStatement st = c.prepareStatement(query);
+                PreparedStatement updateSt = c.prepareStatement(updateQuery)) {
 
-                ResultSet rs = st.executeQuery();
-                while(rs.next()) {
-                    int id = rs.getInt(1);
-                    updateSt.setInt(1, id);
-                    updateSt.executeUpdate();
+                    ResultSet rs = st.executeQuery();
+                    while (rs.next()) {
+                        int id = rs.getInt(1);
+                        updateSt.setInt(1, id);
+                        updateSt.executeUpdate();
+                    }
                 }
-                rs.close();
             } else {
-                PreparedStatement st = c.prepareStatement("DELETE FROM associations WHERE type = " + AssociationType.SHORTCUT + " AND AssociationId NOT IN (SELECT UniqueId FROM associations WHERE (IsDeleted IS NULL OR IsDeleted = 0)) AND (IsDeleted IS NULL OR IsDeleted = 0)");
-                st.executeUpdate();
-                st.close();
+                try(PreparedStatement st = c.prepareStatement("DELETE FROM associations WHERE type = " + AssociationType.SHORTCUT + " AND AssociationId NOT IN (SELECT UniqueId FROM associations WHERE (IsDeleted IS NULL OR IsDeleted = 0)) AND (IsDeleted IS NULL OR IsDeleted = 0)")) {
+                    st.executeUpdate();
+                }
             }
         } catch (SQLException e) {
             throw new SystemException("SQL Feil ved databasekall", e);
