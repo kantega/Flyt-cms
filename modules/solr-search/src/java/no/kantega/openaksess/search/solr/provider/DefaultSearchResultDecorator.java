f5b63754d13a2749ccdc881d8033397f44dc96df
Espen Hjertø <espen.hjerto@kantega.no>
2015-08-05 09:11:34 +0200

    AP-2036 Result fields are available for default search results

--- modules/solr-search/src/java/no/kantega/openaksess/search/solr/provider/DefaultSearchResultDecorator.java
+++ modules/solr-search/src/java/no/kantega/openaksess/search/solr/provider/DefaultSearchResultDecorator.java
@@ -3,6 +3,8 @@ package no.kantega.openaksess.search.solr.provider;
 import no.kantega.search.api.provider.SearchResultDecorator;
 import no.kantega.search.api.search.SearchQuery;
 import no.kantega.search.api.search.SearchResult;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -12,6 +14,7 @@ import java.util.Map;
 @Component
 public class DefaultSearchResultDecorator implements SearchResultDecorator<SearchResult> {
     public static final String DEFAULT_INDEXED_CONTENT_TYPE = "default";
+    private static final Logger log = LoggerFactory.getLogger(DefaultSearchResultDecorator.class);
 
     @Override
     public Collection<String> handledindexedContentTypes() {
@@ -20,12 +23,25 @@ public class DefaultSearchResultDecorator implements SearchResultDecorator<Searc
 
     @Override
     public SearchResult decorate(Map<String, Object> resultMap, String title, String description, SearchQuery query) {
-       return new SearchResult((Integer) resultMap.get("id"),
+        SearchResult result =  new SearchResult((Integer) resultMap.get("id"),
                 (Integer) resultMap.get("securityId"),
                 (String) resultMap.get("indexedContentType"),
                 title,
                 description,
                 (String) resultMap.get("url"),
                 (Integer) resultMap.get("parentId"));
+
+        Map<String, Object> resultFields = result.getResultFields();
+
+
+        for (String resultFieldsItem: query.getResultFields()){
+            try {
+                resultFields.put(resultFieldsItem, resultMap.get(resultFieldsItem));
+            } catch (Exception e){
+                log.error("Unable to decorate result field " + resultFieldsItem);
+            }
+        }
+
+        return result;
     }
 }
