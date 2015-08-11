128d1b156f86f53eb49ac4e029c8f59f6eaf6f11
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2015-08-11 16:47:55 +0200

    AP-2038

--- modules/core/src/java/no/kantega/publishing/jobs/xmlimport/XMLImportJob.java
+++ modules/core/src/java/no/kantega/publishing/jobs/xmlimport/XMLImportJob.java
@@ -18,18 +18,31 @@ package no.kantega.publishing.jobs.xmlimport;
 
 import no.kantega.commons.exception.SystemException;
 import no.kantega.commons.util.XMLHelper;
+import no.kantega.publishing.api.configuration.SystemConfiguration;
 import no.kantega.publishing.api.runtime.ServerType;
 import no.kantega.publishing.api.xmlcache.XMLCacheEntry;
 import no.kantega.publishing.api.xmlcache.XmlCache;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Required;
 import org.w3c.dom.Document;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import javax.annotation.PostConstruct;
+import java.io.IOException;
 import java.util.Objects;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 public class XMLImportJob   {
     private static final Logger log = LoggerFactory.getLogger(XMLImportJob.class);
@@ -42,7 +55,12 @@ public class XMLImportJob   {
     @Autowired
     private ServerType serverType;
 
+    @Autowired
+    private SystemConfiguration configuration;
+
     private ServerType disableForServerType;
+
+    private HttpClientBuilder httpClientBuilder;
 
     public void importXml() {
         if (Objects.equals(serverType, disableForServerType)) {
@@ -52,15 +70,15 @@ public class XMLImportJob   {
 
         log.info( "XMLImport started:" + id + ", url:" + url);
 
-        try {
-            Document xml = XMLHelper.openDocument(new URL(url));
+        try (CloseableHttpResponse execute = httpClientBuilder.build().execute(new HttpGet(url))){
+            Document xml = XMLHelper.openDocument(execute.getEntity().getContent());
 
             if (isValidXML(xml)) {
                 XMLCacheEntry cacheEntry = new XMLCacheEntry(id, xml);
                 xmlCache.storeXMLInCache(cacheEntry);
             }
 
-        } catch (SystemException | MalformedURLException e) {
+        } catch (SystemException | IOException e) {
             log.error("", e);
         }
         log.info( "XMLImport ended:" + id);
@@ -93,8 +111,35 @@ public class XMLImportJob   {
         this.validator = validator;
     }
 
-    public void setDisableForServerType(ServerType disableForServerType) {
-        this.disableForServerType = disableForServerType;
+    @PostConstruct
+    private void init() {
+        int timeout = configuration.getInt("httpclient.connectiontimeout", 10000);
+        String proxyHost = configuration.getString("httpclient.proxy.host");
+        String proxyPort = configuration.getString("httpclient.proxy.port");
+
+        String proxyUser = configuration.getString("httpclient.proxy.username");
+
+        String proxyPassword = configuration.getString("httpclient.proxy.password");
+        if(isNotBlank(proxyHost)) {
+            HttpHost proxy = new HttpHost(proxyHost, Integer.parseInt(proxyPort));
+
+            httpClientBuilder = HttpClients.custom()
+                    .setDefaultRequestConfig(RequestConfig.custom()
+                            .setRedirectsEnabled(true)
+                            .setConnectTimeout(timeout).build())
+                    .setProxy(proxy);
+
+            if(isNotBlank(proxyUser)) {
+                CredentialsProvider credsProvider = new BasicCredentialsProvider();
+                credsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(proxyUser, proxyPassword));
+                httpClientBuilder.setDefaultCredentialsProvider(credsProvider);
+            }
+        } else {
+            httpClientBuilder = HttpClients.custom()
+                    .setDefaultRequestConfig(RequestConfig.custom()
+                            .setRedirectsEnabled(true)
+                            .setConnectTimeout(timeout).build());
+        }
     }
 }
 
