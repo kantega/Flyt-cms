0dee20a28c58ec764a9335470c0e8aec8715861d
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2015-08-11 16:41:17 +0200

    AP-2038

--- modules/core/src/java/no/kantega/publishing/jobs/xmlimport/XMLImportJob.java
+++ modules/core/src/java/no/kantega/publishing/jobs/xmlimport/XMLImportJob.java
@@ -16,20 +16,32 @@
 
 package no.kantega.publishing.jobs.xmlimport;
 
+import no.kantega.commons.configuration.Configuration;
 import no.kantega.commons.exception.SystemException;
 import no.kantega.commons.util.XMLHelper;
 import no.kantega.publishing.api.xmlcache.XMLCacheEntry;
 import no.kantega.publishing.api.xmlcache.XmlCache;
 import no.kantega.publishing.common.Aksess;
 import no.kantega.publishing.common.data.enums.ServerType;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.client.HttpClients;
 import org.quartz.JobExecutionException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 import org.w3c.dom.Document;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.io.IOException;
+
+import static org.apache.commons.lang3.StringUtils.isNotBlank;
 
 public class XMLImportJob  extends QuartzJobBean {
     private static final Logger log = LoggerFactory.getLogger(XMLImportJob.class);
@@ -38,8 +50,12 @@ public class XMLImportJob  extends QuartzJobBean {
     private XMLImportValidator validator = new DefaultXMLImportValidator();
     private XmlCache xmlCache;
 
+    private HttpClientBuilder httpClientBuilder;
+    private final int CONNECTION_TIMEOUT = 10000;
+
     protected void executeInternal(org.quartz.JobExecutionContext jobExecutionContext) throws org.quartz.JobExecutionException {
         xmlCache = (XmlCache)jobExecutionContext.getMergedJobDataMap().get("xmlCache");
+        init();
         if (Aksess.getServerType() == ServerType.SLAVE) {
             log.info( "Job is disabled for server type slave");
             return;
@@ -51,15 +67,15 @@ public class XMLImportJob  extends QuartzJobBean {
         }
         log.info( "XMLImport started:" + id + ", url:" + url);
 
-        try {
-            Document xml = XMLHelper.openDocument(new URL(url));
+        try (CloseableHttpResponse execute = httpClientBuilder.build().execute(new HttpGet(url))){
+            Document xml = XMLHelper.openDocument(execute.getEntity().getContent());
 
             if (isValidXML(xml)) {
                 XMLCacheEntry cacheEntry = new XMLCacheEntry(id, xml);
                 xmlCache.storeXMLInCache(cacheEntry);
             }
 
-        } catch (SystemException | MalformedURLException e) {
+        } catch (SystemException | IOException e) {
             log.error("", e);
         }
         log.info( "XMLImport ended:" + id);
@@ -88,6 +104,36 @@ public class XMLImportJob  extends QuartzJobBean {
 
     public void setValidator(XMLImportValidator validator) {
         this.validator = validator;
+    }
+
+    private void init() {
+        Configuration configuration = Aksess.getConfiguration();
+        String proxyHost = configuration.getString("linkchecker.proxy.host");
+        String proxyPort = configuration.getString("linkchecker.proxy.port");
+
+        String proxyUser = configuration.getString("linkchecker.proxy.username");
+
+        String proxyPassword = configuration.getString("linkchecker.proxy.password");
+        if(isNotBlank(proxyHost)) {
+            HttpHost proxy = new HttpHost(proxyHost, Integer.parseInt(proxyPort));
+
+            httpClientBuilder = HttpClients.custom()
+                    .setDefaultRequestConfig(RequestConfig.custom()
+                            .setRedirectsEnabled(true)
+                            .setConnectTimeout(CONNECTION_TIMEOUT).build())
+                    .setProxy(proxy);
+
+            if(isNotBlank(proxyUser)) {
+                CredentialsProvider credsProvider = new BasicCredentialsProvider();
+                credsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(proxyUser, proxyPassword));
+                httpClientBuilder.setDefaultCredentialsProvider(credsProvider);
+            }
+        } else {
+            httpClientBuilder = HttpClients.custom()
+                    .setDefaultRequestConfig(RequestConfig.custom()
+                            .setRedirectsEnabled(true)
+                            .setConnectTimeout(CONNECTION_TIMEOUT).build());
+        }
     }
 }
 
