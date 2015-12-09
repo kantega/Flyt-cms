51a60ad9328c91b92dac30cbb0571450b8b685b0
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2015-12-09 09:16:09 +0100

    Remove public from methods in PermissionAO.java

--- modules/core/src/java/no/kantega/publishing/api/services/security/PermissionAO.java
+++ modules/core/src/java/no/kantega/publishing/api/services/security/PermissionAO.java
@@ -11,18 +11,18 @@ public interface PermissionAO {
      * @param object permissions are wanted for.
      * @return all permission defined on the given <code>BaseObject</code>.
      */
-    public List<Permission> getPermissions(BaseObject object);
+    List<Permission> getPermissions(BaseObject object);
 
     /**
      * @return Overview of all Permissions for the given <code>no.kantega.publishing.common.data.enums.ObjectType</code>
      * @see no.kantega.publishing.common.data.enums.ObjectType
      */
-    public List<ObjectPermissionsOverview> getPermissionsOverview(int objectType);
+    List<ObjectPermissionsOverview> getPermissionsOverview(int objectType);
 
     /**
      * Set <code>Permission</code>s for a <code>BaseObject</code>
      * @param object - The <code>BaseObject</code> to set permissions for.
      * @param permissions - The permissions to set.
      */
-    public void setPermissions(BaseObject object, List<Permission> permissions);
+    void setPermissions(BaseObject object, List<Permission> permissions);
 }
