5e5dfdf2fbb4b86db49c071411ab944d0cb66317
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2016-07-05 14:59:19 +0200

    Do not throw when transforming string to int (cherry picked from commit f1c811e)

--- modules/core/src/java/no/kantega/publishing/jobs/alerts/UnusedAttachmentsFinder.java
+++ modules/core/src/java/no/kantega/publishing/jobs/alerts/UnusedAttachmentsFinder.java
@@ -1,32 +1,33 @@
 package no.kantega.publishing.jobs.alerts;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
 import no.kantega.commons.exception.NotAuthorizedException;
-import no.kantega.publishing.api.attachment.ao.AttachmentAO;
 import no.kantega.publishing.api.content.ContentIdentifier;
-import no.kantega.publishing.api.link.LinkDao;
 import no.kantega.publishing.common.Aksess;
+import no.kantega.publishing.common.ao.AttachmentAO;
+import no.kantega.publishing.common.ao.LinkDao;
 import no.kantega.publishing.common.data.Attachment;
 import no.kantega.publishing.common.service.ContentManagementService;
+import no.kantega.publishing.modules.linkcheck.check.LinkHandler;
 import no.kantega.publishing.modules.linkcheck.check.LinkOccurrence;
 import no.kantega.publishing.modules.linkcheck.check.LinkQueryGenerator;
 import no.kantega.publishing.modules.linkcheck.crawl.LinkEmitter;
 import no.kantega.publishing.security.SecuritySession;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Objects;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
-import static java.util.Objects.nonNull;
+import static com.google.common.collect.Collections2.filter;
+import static com.google.common.collect.Collections2.transform;
+
 
 public class UnusedAttachmentsFinder {
-
-    @Autowired
-    private AttachmentAO attachmentAO;
 
     @Autowired
     private LinkDao linkDao;
@@ -37,23 +38,30 @@ public class UnusedAttachmentsFinder {
     private static final Pattern attachmentPattern = Pattern.compile("(.*/attachment.ap\\?id=(?<apId>\\d+).*)|(.*/attachment/(?<prettyId>\\d+).*)");
 
     public List<Attachment> getUnusedAttachments() {
-        List<Integer> referredAttachments = new LinkedList<>();
+        final List<Integer> referredAttachments = new LinkedList<>();
 
         linkDao.doForEachLink(
-                () -> new LinkQueryGenerator.Query("select * from link where url like '%" + Aksess.VAR_WEB + "/attachment%'"),
-                (linkId, url, occurrence) -> {
-                    String attachmentId = getAttachmentId(url);
+                new LinkQueryGenerator() {
+                    @Override
+                    public Query getQuery() {
+                        return new Query("select * from link where url like '%" + Aksess.VAR_WEB + "/attachment%'");
+                    }
+                },
+                new LinkHandler() {
+                    public void handleLink(int id, String link, LinkOccurrence occurrence) {
+                        String attachmentId = getAttachmentId(link);
 
-                    if(nonNull(attachmentId)) {
-                        referredAttachments.add(Integer.parseInt(attachmentId));
+                        if (attachmentId != null) {
+                            referredAttachments.add(Integer.parseInt(attachmentId));
 
+                        }
                     }
                 });
-        List<Integer> allAttachmentIds = attachmentAO.getAllAttachmentIds();
+        List<Integer> allAttachmentIds = AttachmentAO.getAllAttachmentIds();
 
         allAttachmentIds.removeAll(referredAttachments);
 
-        return attachmentAO.getAttachments(allAttachmentIds);
+        return AttachmentAO.getAttachments(allAttachmentIds);
     }
 
     private String getAttachmentId(String url) {
@@ -75,24 +83,30 @@ public class UnusedAttachmentsFinder {
             linkDao.deleteLinksForContentId(contentId);
             ContentIdentifier cid = ContentIdentifier.fromContentId(contentId);
             linkDao.saveLinksForContent(emitter, cms.getContent(cid, false));
-            Collection<LinkOccurrence> attachmentUris = linkDao.getLinksforContentId(contentId)
-                    .stream()
-                    .filter(lo -> lo.getUrl().contains("/attachment"))
-                    .collect(Collectors.toList());
+            Collection<LinkOccurrence> attachmentUris = filter(linkDao.getLinksforContentId(contentId), new Predicate<LinkOccurrence>() {
+                @Override
+                public boolean apply(LinkOccurrence lo) {
+                    return lo.getUrl().contains("/attachment");
+                }
+            });
+            Collection<Integer> attachmentIds = transform(attachmentUris, new Function<LinkOccurrence, Integer>() {
+                @Override
+                public Integer apply(LinkOccurrence input) {
+                    String attachmentId = getAttachmentId(input.getUrl());
+                    if (attachmentId == null) {
+                        return null;
+                    } else {
+                        return Integer.parseInt(attachmentId);
+                    }
+                }
+            });
 
-            Collection<Integer> attachmentIds =  attachmentUris
-                    .stream()
-                    .map(lo -> getAttachmentId(lo.getUrl()))
-                    .filter(Objects::nonNull)
-                    .map(Integer::parseInt)
-                    .collect(Collectors.toList());
-
-
-            List<Integer> attachmentsForContent = attachmentAO.getAttachmentList(cid)
-                    .stream()
-                    .map(Attachment::getId)
-                    .collect(Collectors.toList());
-
+            List<Integer> attachmentsForContent = new ArrayList<>(transform(AttachmentAO.getAttachmentList(cid), new Function<Attachment, Integer>() {
+                @Override
+                public Integer apply(Attachment input) {
+                    return input.getId();
+                }
+            }));
             attachmentsForContent.removeAll(attachmentIds);
             return attachmentsForContent;
         } catch (NotAuthorizedException e) {
