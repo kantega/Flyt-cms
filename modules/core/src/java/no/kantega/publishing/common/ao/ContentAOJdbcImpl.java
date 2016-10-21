edbb1cd640fe6a06439d4373167ea54ee74da7a2
Marvin Bredal Lillehaug <marvin.lillehaug@kantega.no>
2016-10-21 14:59:11 +0200

    Do not duplicate attributes
    (cherry picked from commit ec62750)

--- modules/core/src/java/no/kantega/publishing/common/ao/ContentAOJdbcImpl.java
+++ modules/core/src/java/no/kantega/publishing/common/ao/ContentAOJdbcImpl.java
@@ -74,7 +74,6 @@ public class ContentAOJdbcImpl extends NamedParameterJdbcDaoSupport implements C
     @Override
     public ContentIdentifier deleteContent(ContentIdentifier cid) {
         contentIdHelper.assureContentIdAndAssociationIdSet(cid);
-        ContentIdentifier parent = getParent(cid);
 
         int id = cid.getContentId();
         // Slett tilgangsrettigheter
@@ -105,7 +104,7 @@ public class ContentAOJdbcImpl extends NamedParameterJdbcDaoSupport implements C
         jdbcTemplate.update("delete from contentversion where ContentId = ?", id);
         jdbcTemplate.update("delete from content where ContentId = ?", id);
 
-        return parent;
+        return getParent(cid);
     }
 
     @Override
